50b830c4b4c80c2a03dd4b08068ecba4771357b5
Shain Collins <shainc@j-ldimensional.com>
2023-04-28 11:05:03 -0500

    Updated it to actually work better

--- scripts/automulticlick.py
+++ scripts/automulticlick.py
@@ -3,31 +3,45 @@ import threading
 import time
 import keyboard
 
-# Define a function that simulates a mouse click at a given location with a given time interval
-def click_location_with_interval(location, interval):
-    time.sleep(interval)
+# Define a function that simulates a mouse click at a given location
+def click_location(location):
     pyautogui.moveTo(location[0], location[1])
     pyautogui.click()
 
 # Define a list of target locations and time intervals
-locations_and_intervals = [((100, 100), 1), ((200, 200), 2), ((300, 300), 0.5), ((400, 400), 1.5), ((500, 500), 0.5)]
+locations_and_intervals = [((500, 500), 1), ((500, 600), 2)]
 
-# Loop indefinitely until the "q" key is pressed
-while True:
-    # Loop through the list of target locations and time intervals, and create a thread for each location
-    threads = []
-    for location, interval in locations_and_intervals:
-        thread = threading.Thread(target=click_location_with_interval, args=[location, interval])
-        threads.append(thread)
+# Define a function that schedules the execution of a mouse click simulation
+def schedule_click(location, interval, stop_event):
+    while not stop_event.is_set():
+        click_location(location)
+        stop_event.wait(interval)
 
-    # Start all the threads
-    for thread in threads:
-        thread.start()
+# Create an event object to signal all threads to stop
+stop_event = threading.Event()
 
-    # Wait for all the threads to finish
-    for thread in threads:
-        thread.join()
+# Create a list to store all threads
+threads = []
 
-    # Check if the "q" key has been pressed, and break out of the loop if it has
-    if keyboard.is_pressed('q'):
-        break
+# Schedule the execution of the mouse click simulation for each target location
+for location, interval in locations_and_intervals:
+    t = threading.Thread(target=schedule_click, args=(location, interval, stop_event))
+    t.start()
+    threads.append(t)
+
+# Define a function to stop the program
+def stop_program():
+    stop_event.set()
+
+# Register the "q" key as a hotkey to stop the program
+keyboard.add_hotkey('q', stop_program)
+
+# Wait for all threads to finish
+for t in threads:
+    t.join()
+
+# Stop the keyboard listener
+keyboard.unhook_all()
+
+# Print a message to confirm that the program has stopped
+print("Program stopped.")
